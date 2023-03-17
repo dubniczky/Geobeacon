7d31330315e5aa0893f3c59b4e93844218b65a12
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-03-17 21:28:31 +0100

    Updated server with unique URLs

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ app.use(express.static('public'))
 app.use(morgan(':date[iso] :remote-addr :method :url :status :res[content-length] - :response-time ms'))
 app.enable("trust proxy")
 
+app.get("/images/:id", async (req, res) => {
+    const id = req.params.id
+
+    console.log(`${id} - ${req.ip}`)
+
+    res.sendFile('public/cat.jpg', { root: __dirname })
+})
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 })
